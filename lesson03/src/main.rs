a7ec2b41c06dfd273973cdfe8646ec93af75dcd3
Prince Muel <vansomecsam@gmail.com>
2024-02-03 05:26:31 +0100

    feat: learn about variable shadowing
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- lesson03/src/main.rs
+++ lesson03/src/main.rs
@@ -1,8 +1,26 @@
-const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+// fn main() {
+//     let x = 5;
+
+//     let x = x + 1;
+
+//     {
+//         let x = x * 2;
+//         println!("The value of x in the inner scope is: {x}");
+//     }
+
+//     println!("The value of x is: {x}");
+// }
 
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {x}");
-    x = 6;
-    println!("The value of x is: {x}");
+    let mut x: u32 = 1;
+
+    {
+        let mut x = x;
+
+        x += 2;
+
+        println!("{x}");
+    }
+
+    println!("{x}");
 }
