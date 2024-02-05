e816501e1a2bea801d5283d467ecb3fd5bd6f2d8
Prince Muel <vansomecsam@gmail.com>
2024-02-05 02:06:52 +0100

    chore: remove unused files
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- lesson03/src/main.rs
+++ lesson03/src/main.rs
@@ -11,16 +11,80 @@
 //     println!("The value of x is: {x}");
 // }
 
-fn main() {
-    let mut x: u32 = 1;
+// fn main() {
+//     let mut x: u32 = 1;
 
-    {
-        let mut x = x;
+//     {
+//         let mut x = x;
 
-        x += 2;
+//         x += 2;
 
-        println!("{x}");
-    }
+//         println!("{x}");
+//     }
 
-    println!("{x}");
-}
+//     println!("{x}");
+// }
+
+// fn main() {
+//     let x = 2.0; // f64
+
+//     let y: f32 = 3.0; // f32
+// }
+
+// fn main() {
+//     // addition
+//     let sum = 5 + 10;
+
+//     // subtraction
+//     let difference = 95.5 - 4.3;
+
+//     // multiplication
+//     let product = 4 * 30;
+
+//     // division
+//     let quotient = 56.7 / 32.2;
+//     let truncated = -5 / 3; // Results in -1
+
+//     // remainder
+//     let remainder = 43 % 5;
+// }
+
+// fn main() {
+//     let t = true;
+
+//     let f = false; // with explicit type annotation
+// }
+
+// fn main() {
+//     let c = 'z';
+//     let z: char = 'ℤ'; // with explicit type annotation
+//     let heart_eyed_cat = '😻';
+// }
+
+// fn main() {
+//     let x = 2.0;
+
+//     println!("{x}");
+// }
+
+// fn main() {
+//     let tup: (i32, f64, u8) = (500, 6.4, 1);
+// }
+
+// fn main() {
+//     let x: (i32, f64, u8) = (500, 6.4, 1);
+
+//     let five_hundred = x.0;
+
+//     let six_point_four = x.1;
+
+//     let one = x.2;
+// }
+
+// fn main() {
+//     let tup = (500, 6.4, 1);
+
+//     let (x, y, z) = tup;
+
+//     println!("The value of y is: {y}");
+// }
