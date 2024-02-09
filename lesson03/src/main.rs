69df699c15f40b68bcfdf95392381eb4409e4688
Prince Muel <vansomecsam@gmail.com>
2024-02-09 03:37:52 +0100

    feat: do temp conversions, fib sequence
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- lesson03/src/main.rs
+++ lesson03/src/main.rs
@@ -1,17 +1,46 @@
-// fn main() {
-//     println!("Hello, world!");
-
-//     another_function();
-// }
-
-// fn another_function() {
-//     println!("Another function.");
-// }
-
 fn main() {
-    another_function(5);
+    let deg = 100.0;
+    let fahrenheit = c_to_f(deg);
+    let celsius = f_to_c(deg);
+    let n = 9;
+
+    println!("The value of {deg}C in fahrenheit is {fahrenheit}");
+    println!("The value of {deg}F in celsius is {celsius}");
+    println!("The {n}th Fibonacci number: {}", fib(n))
 }
 
-fn another_function(x: i32) {
-    println!("The value of x is: {x}");
+fn c_to_f(value: f64) -> f64 {
+    (value * (9.0 / 5.0)) + 32.0
+}
+
+fn f_to_c(value: f64) -> f64 {
+    (value - 32.0) * (5.0 / 9.0)
+}
+
+// fn fib(n: i32) -> i32 {
+//     if n <= 1 {
+//         return n;
+//     }
+//     return fib(n - 1) + fib(n - 2);
+// }
+
+fn fib(n: i32) -> i32 {
+    let mut a = 0;
+    let mut b = 1;
+    let mut c: i32;
+    let mut index = 2;
+
+    if n == 0 {
+        return a;
+    }
+
+    while index <= n {
+        c = a + b;
+        a = b;
+        b = c;
+
+        index += 1
+    }
+
+    return b;
 }
