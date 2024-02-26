6b9f4072672baeff33b923e81ec14f34095cdb21
Prince Muel <vansomecsam@gmail.com>
2024-02-26 03:00:09 +0100

    feat: update guessing game

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,36 @@
+use colored::Colorize;
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Please input your guess...");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to readline");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(value) => value,
+            Err(_) => continue,
+        };
+
+        println!("Your guess is: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{}", "Bah! It's too small!".red()),
+            Ordering::Greater => println!("{}", "Urgh! It's too big!".red()),
+            Ordering::Equal => {
+                println!("{}", "Hoorah! You win!".green());
+                break;
+            }
+        };
+    }
+}
