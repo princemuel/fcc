ffd256633d9e3adef0caee06be4b1fac3276ec53
Prince Muel <vansomecsam@gmail.com>
2024-09-04 14:27:50 +0100

    feat: learn common programming concepts

--- lesson03/src/main.rs
+++ lesson03/src/main.rs
@@ -126,17 +126,9 @@
 //     println!("{}", a[0] + t.1[0]); // 1 + 3 = 4
 // }
 
-struct Person {
-    age: u8,
-    first_name: String,
-    last_name: String,
-}
-enum TrafficLight {
-    Red,
-    Yellow,
-    Green,
-}
+use std::collections::HashMap;
 
+#[allow(warnings)]
 fn main() {
     // INTEGERS
     let small_num_01: u8 = 255;
@@ -188,6 +180,12 @@ fn main() {
     let is_false: bool = false;
     println!("is_true = {}, is_false = {}", is_true, is_false);
 
+    let age: u8 = 18;
+
+    if age >= 18 {
+    } else {
+    }
+
     if is_true {
         println!("is_true is true");
     } else {
@@ -222,13 +220,46 @@ fn main() {
     let _byte = [0; 8];
     let first = array[0];
     let second = array[1];
+    println!("Number Array: {:?}", array);
     println!("first {}, second {}", first, second);
 
     for element in array.iter() {
         println!("element: {}", element);
     }
 
+    // SLICES
+    let number_slices = &[1, 2, 3, 4, 5];
+    println!("Number Slices: {:?}", number_slices);
+
+    let animal_slices = &["Lion", "Buffalo", "Squirrel"];
+    println!("Animal Slices: {:?}", animal_slices);
+
+    let book_slices = &[
+        &"Harry Potter".to_string(),
+        &"Inheritance Games".to_string(),
+        &"Hunger Games".to_string(),
+    ];
+    println!("Book Slices: {:?}", book_slices);
+
+    // Strings vs String Slices (&str)
+    // A- Strings [growable(grow/shrink), mutable(add,delete), owned]
+    let mut stone_cold = String::from("Hell, ");
+    println!("Stone Cold Says: {}", stone_cold);
+    stone_cold.push_str("Yeah!");
+
+    // B- &str (String Slices... specific size, immutable, reference to another)
+    let string = String::from("Hello, World");
+    let slice: &str = &string[0..5];
+    // let slice: &str = &string;
+    println!("Slice Value: {}", slice);
+
     // STRUCTS
+    struct Person {
+        age: u8,
+        first_name: String,
+        last_name: String,
+    };
+
     let person = Person {
         first_name: String::from("Samuel"),
         last_name: "Chukwuzube".to_owned(),
@@ -239,7 +270,90 @@ fn main() {
         person.first_name, person.last_name, person.age
     );
 
+    struct Book {
+        title: String,
+        author: String,
+        pages: u32,
+        available: bool,
+    }
+
+    struct User {
+        active: bool,
+        username: String,
+        email: String,
+        sign_in_count: u64,
+    }
+
+    let mut elon = User {
+        active: true,
+        username: String::from("elon"),
+        email: String::from("elon@example.com"),
+        sign_in_count: 1,
+    };
+    elon.email = String::from("sam@hotmail.com");
+
+    println!("User email is {}", elon.email);
+
+    fn build_user(email: String, username: String) -> User {
+        User {
+            active: true,
+            username,
+            email,
+            sign_in_count: 1,
+        }
+    };
+
+    let mark = User {
+        username: String::from("mark"),
+        email: String::from("mark@example.com"),
+        ..elon
+    };
+
+    // Tuple STRUCTS
+    struct Color(i32, i32, i32);
+    struct Point(i32, i32, i32);
+    let _white = Color(255, 255, 255);
+
+    // Unit-like STRUCTS
+    struct AlwaysEqual;
+    let _subject = AlwaysEqual;
+
     // ENUMS
+
+    enum IpAddressKind {
+        V4,
+        V6,
+    }
+
+    enum IpAddress {
+        V4(u8, u8, u8, u8), // enhanced enums
+        V6(String),
+    }
+    struct IPAddress {
+        kind: IpAddressKind,
+        address: String,
+    }
+
+    let _v4 = IpAddressKind::V4;
+    let _v6 = IpAddressKind::V6;
+
+    fn route(ip_kind: IpAddressKind) {}
+
+    route(IpAddressKind::V6);
+
+    let home = IPAddress {
+        kind: IpAddressKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let home = IpAddress::V4(127, 0, 0, 1);
+    let loopback = IpAddress::V6(String::from("::1"));
+
+    enum TrafficLight {
+        Red,
+        Yellow,
+        Green,
+    }
     let traffic_light = TrafficLight::Red;
 
     match traffic_light {
@@ -255,4 +369,52 @@ fn main() {
     };
 
     println!("The current traffic light says {}", current_light);
+
+    tell_height(182);
+    human_id("Samuel", 28, 182.2);
+
+    let _x = {
+        let price = 5;
+        let qty = 10;
+        price * qty
+    };
+
+    let weight = 70.0;
+    let height = 1.82;
+    let bmi = calculate_bmi(weight, height);
+
+    println!("Your BMI is: {:.2}", bmi);
+
+    let mut v1: Vec<i32> = Vec::new();
+    let mut v2 = vec![1, 2, 3, 4];
+    v1.push(1);
+    v2.push(5);
+
+    // ---------------------- HASHMAPS ---------------------------  //
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("blue"), 10);
+    scores.insert(String::from("red"), 50);
+
+    let team_name = String::from("yellow");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+
+    for (key, value) in scores {
+        println!("{key}: {value}");
+    }
+}
+
+fn tell_height(height: u32) {
+    println!("My height is {}cm.", height);
+}
+
+fn human_id(name: &str, age: u32, height: f32) {
+    println!(
+        "My name is {}. I am {} years old, and my height is {}cm.",
+        name, age, height
+    )
+}
+
+fn calculate_bmi(weight_kg: f64, height_m: f64) -> f64 {
+    weight_kg / (height_m * height_m)
 }
