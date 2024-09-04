aa42ee822df4325da5076faf241a0e50513c5ce0
Prince Muel <vansomecsam@gmail.com>
2024-09-04 14:40:27 +0100

    feat: add generics with comparison
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- lesson10/src/main.rs
+++ lesson10/src/main.rs
@@ -1,12 +1,37 @@
+struct Point {}
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
+    let largest = get_largest_01(&number_list);
+    println!("The largest number is: {largest}");
 
+    let number_list = vec![100, 34, 6000, 89, 54, 2, 43, 8];
+    let largest = get_largest_01(&number_list);
+    println!("The largest number is: {largest}");
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let largest = get_largest_02(&char_list);
+    println!("The largest char is: {largest}");
+}
+
+fn get_largest_02<T: PartialOrd + Copy>(number_list: &[T]) -> T {
     let mut largest = number_list[0];
 
     for number in number_list {
-        if number > largest {
-            largest = number;
+        if number > &largest {
+            largest = *number;
         }
     }
-    println!("The largest number is: {largest}");
+    largest
+}
+
+fn get_largest_01(number_list: &[i32]) -> i32 {
+    let mut largest = number_list[0];
+
+    for number in number_list {
+        if number > &largest {
+            largest = *number;
+        }
+    }
+    largest
 }
