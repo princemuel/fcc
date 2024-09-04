2ede51ed43f730a13602f3cf1bc55824cb4d6668
Prince Muel <vansomecsam@gmail.com>
2024-09-04 14:19:31 +0100

    perf: improve guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,6 +3,25 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Self {
+        // if value < 1 || value > 100 {
+        if !(1..=100).contains(&value) {
+            panic!("Your guess must be between 1 and 100, got {value}.");
+        }
+
+        Self { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..=100);
@@ -11,15 +30,15 @@ fn main() {
         println!("Please input your guess...");
         let mut guess = String::new();
 
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to readline");
+        io::stdin().read_line(&mut guess).expect("Failed to readline");
 
         // using the 'turbofish' syntax
-        let guess = match guess.trim().parse::<u32>() {
+        let guess = match guess.trim().parse::<i32>() {
             Ok(num) => num,
             Err(_) => continue,
         };
+
+        let guess = Guess::new(guess).value();
 
         println!("Your guess is: {}", guess);
 
@@ -29,7 +48,7 @@ fn main() {
             Ordering::Equal => {
                 println!("{}", "Hoorah! You win!".green());
                 break;
-            }
+            },
         };
     }
 }
