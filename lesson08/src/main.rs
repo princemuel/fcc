ae0781fe0bd670c3892e74df4aabae007c560cc0
Prince Muel <vansomecsam@gmail.com>
2024-09-04 14:20:36 +0100

    feat: learn more collection types

--- lesson08/src/main.rs
+++ lesson08/src/main.rs
@@ -1,3 +1,7 @@
+use std::collections::HashMap;
+use std::io::Bytes;
+use unicode_segmentation::UnicodeSegmentation;
+
 fn main() {
     let a = [1, 2, 3];
 
@@ -31,28 +35,28 @@ fn main() {
         println!("{}", idx);
     }
 
-    enum SpreadsheetCell {
+    enum CellValue {
         Int(i32),
         Float(f64),
         Text(String),
     }
 
-    let row = vec![
-        SpreadsheetCell::Int(3),
-        SpreadsheetCell::Text(String::from("blue")),
-        SpreadsheetCell::Float(10.12),
+    let row = [
+        CellValue::Int(3),
+        CellValue::Text(String::from("blue")),
+        CellValue::Float(10.12),
     ];
 
     match &row[1] {
-        SpreadsheetCell::Int(value) => println!("{}", value),
+        CellValue::Int(value) => println!("{}", value),
         _ => println!("Invalid Integer!"),
     }
 
     // Strings are stored as a collection of UTF-8 encoded bytes
-    let s1 = String::new();
+    let _s1 = String::new();
     let s2 = "initial contents";
-    let s3 = s2.to_string();
-    let s4 = String::from("initial contents");
+    let _s3 = s2.to_string();
+    let _s4 = String::from("initial contents");
 
     let mut s = String::from("foo");
     s.push_str("bar");
@@ -65,4 +69,45 @@ fn main() {
 
     let hello = String::from("Hello");
     // let c = hello[0];
+    for byte in hello.bytes() {
+        println!("{byte}");
+    }
+    for char in hello.chars() {
+        println!("{char}");
+    }
+    for grapheme in hello.graphemes(true) {
+        println!("{grapheme}");
+    }
+
+    let blue = String::from("Blue");
+    let yellow = String::from("Yellow");
+
+    let mut scores = HashMap::new();
+
+    scores.insert(blue, 10);
+    scores.insert(yellow, 50);
+
+    let team_name = String::from("Blue");
+    let _score = scores.get(&team_name);
+
+    for (key, value) in &scores {
+        println!("{key}: {value}");
+    }
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Blue"), 20); // overwrites previous
+
+    scores.entry(String::from("Yellow")).or_insert(30);
+    scores.entry(String::from("Yellow")).or_insert(40);
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{map:?}");
 }
