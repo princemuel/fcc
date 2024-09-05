6a517002c162ea7378c3daaed896886f17beca7a
Prince Muel <vansomecsam@gmail.com>
2024-09-05 20:53:40 +0100

    feat: init memoizer struct
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- lesson13/src/main.rs
+++ lesson13/src/main.rs
@@ -1,4 +1,62 @@
+use std::thread::sleep;
+use std::time::Duration;
+
 //
 fn main() {
-    println!("Hello, world!");
+    let intensity = 10;
+    let random_num = 7;
+    generate_workout(intensity, random_num);
+}
+
+pub fn expensive_calculation(intensity: u16) -> u16 {
+    println!("calculating slowly...");
+    sleep(Duration::from_secs(3));
+    intensity
+}
+
+struct Memoizer<T>
+where
+    T: Fn(u16) -> u16,
+{
+    calculation: T,
+    result: Option<u16>,
+}
+
+impl<T> Memoizer<T>
+where
+    T: Fn(u16) -> u16,
+{
+    pub fn new(calculation: T) -> Self {
+        Self { calculation, result: None }
+    }
+
+    fn value(&mut self, arg: u16) -> u16 {
+        match self.result {
+            Some(value) => value,
+            None => {
+                let value = (self.calculation)(arg);
+                self.result = Some(value);
+                value
+            },
+        }
+    }
+}
+
+pub fn generate_workout(intensity: u16, random_num: u16) {
+    // let expensive_result = expensive_calculation(intensity);
+
+    let expensive_closure = |value| {
+        println!("calculating slowly...");
+        sleep(Duration::from_secs(3));
+        value
+    };
+
+    if intensity < 25 {
+        println!("Today, go {} pushups", expensive_closure(intensity));
+        println!("Next, do {} situps", expensive_closure(intensity));
+    } else if random_num == 3 {
+        println!("Take a break today. Remember to stay hydrated");
+    } else {
+        println!("Today, run for {} minutes", expensive_closure(intensity));
+    }
 }
