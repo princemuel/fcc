b4f3e1f6d880f8eecb54e06ee17b29f679f1682c
Prince Muel <vansomecsam@gmail.com>
2024-09-05 19:18:09 +0100

    feat: create parse config fn

--- minigrepper/src/main.rs
+++ minigrepper/src/main.rs
@@ -3,11 +3,25 @@ use std::{env, fs};
 fn main() {
     let args = env::args().collect::<Vec<String>>();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let Config { query, filename } = parse_config(&args);
+
     println!("Searching for {query}");
     println!("In file {filename:?}");
 
-    let _contents =
+    let contents =
         fs::read_to_string(filename).expect("something went wrong while reading the file");
+
+    println!("With text:\n{contents}");
+}
+
+pub struct Config {
+    query: String,
+    filename: String,
+}
+
+pub fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
