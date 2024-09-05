6869f31c9ef74b5e1aab638787aa8acb7551c879
Prince Muel <vansomecsam@gmail.com>
2024-09-05 18:49:21 +0100

    feat: init minigrep project

--- minigrepper/src/main.rs
+++ minigrepper/src/main.rs
@@ -1,1 +1,13 @@
-fn main() {}
+use std::{env, fs};
+
+fn main() {
+    let args = env::args().collect::<Vec<String>>();
+
+    let query = &args[1];
+    let filename = &args[2];
+    println!("Searching for {query}");
+    println!("In file {filename:?}");
+
+    let _contents =
+        fs::read_to_string(filename).expect("something went wrong while reading the file");
+}
