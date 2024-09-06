416105fe96780a72310eb9e270ef3f1f3ce7ed7e
Prince Muel <vansomecsam@gmail.com>
2024-09-06 19:55:24 +0100

    feat: learn about iterators

--- lesson14/src/main.rs
+++ lesson14/src/main.rs
@@ -1,3 +1,114 @@
 fn main() {
-    println!("Hello, world!");
+    let v1 = [1, 2, 3];
+    let v1_iter = v1.iter();
+    for value in v1_iter {
+        println!("Got {value}");
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Shoe {
+    size: u8,
+    style: String,
+}
+
+pub fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u8) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
+
+pub struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    pub fn new() -> Self {
+        Self { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn iterator_demo() {
+        let v1 = [1, 2, 3];
+        let mut v1_iter = v1.iter();
+
+        assert_eq!(v1_iter.next(), Some(&1));
+        assert_eq!(v1_iter.next(), Some(&2));
+        assert_eq!(v1_iter.next(), Some(&3));
+        assert_eq!(v1_iter.next(), None);
+    }
+
+    #[test]
+    fn iterator_sum() {
+        let v1 = [1, 2, 3];
+        let v1_iter = v1.iter();
+
+        let total: i32 = v1_iter.sum();
+
+        assert_eq!(total, 6);
+    }
+
+    #[test]
+    fn iterator_map() {
+        let v1 = [1, 2, 3];
+        let v2: Vec<_> = v1.iter().map(|value| value + 1).collect();
+        assert_eq!(v2, vec![2, 3, 4]);
+    }
+
+    #[test]
+    fn filters_by_size() {
+        let shoes = vec![
+            Shoe { size: 10, style: String::from("sneaker") },
+            Shoe { size: 13, style: String::from("sandal") },
+            Shoe { size: 10, style: String::from("boot") },
+        ];
+
+        let in_my_size = shoes_in_my_size(shoes, 10);
+
+        assert_eq!(
+            in_my_size,
+            vec![
+                Shoe { size: 10, style: String::from("sneaker") },
+                Shoe { size: 10, style: String::from("boot") },
+            ]
+        )
+    }
+
+    #[test]
+    fn call_next_directly() {
+        let mut counter = Counter::new();
+
+        assert_eq!(counter.next(), Some(1));
+        assert_eq!(counter.next(), Some(2));
+        assert_eq!(counter.next(), Some(3));
+        assert_eq!(counter.next(), Some(4));
+        assert_eq!(counter.next(), Some(5));
+        assert_eq!(counter.next(), None);
+    }
+
+    #[test]
+    fn use_other_iter_trait_methods() {
+        let sum: u32 = Counter::new()
+            .zip(Counter::new().skip(1))
+            .map(|(a, b)| a * b)
+            .filter(|x| x % 3 == 0)
+            .sum();
+        assert_eq!(18, sum);
+    }
 }
