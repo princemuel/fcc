318f0319223ca177fb173eb3df6470477ffd1efb
Prince Muel <vansomecsam@gmail.com>
2024-09-07 21:35:02 +0100

    feat: impl ref countting for List enum

--- lesson22/src/main.rs
+++ lesson22/src/main.rs
@@ -1,10 +1,13 @@
 use core::str;
+use std::cell::RefCell;
 use std::{ops::Deref, rc::Rc};
 
+#[derive(Debug)]
 pub enum List {
-    // Cons(i32, Box<List>),
-    Cons(i32, Rc<List>),
+    Cons(Rc<RefCell<i32>>, Rc<List>),
     Nil,
+    // Cons(i32, Box<List>),
+    // Cons(i32, Rc<List>),
 }
 
 struct MyBox<T>(T);
@@ -53,13 +56,13 @@ where
     pub fn set_value(&mut self, value: usize) {
         self.value = value;
 
-        let percentage_of_max = (self.value / self.max) as f64;
+        let percentage_of_max = self.value as f64 / self.max as f64;
 
         if percentage_of_max >= 1.0 {
             self.messenger.send("Error: You are over your quota!");
         } else if percentage_of_max >= 0.9 {
             self.messenger
-                .send("Urgent Warning: You've used up over 90% of your quota!");
+                .send("Urgent warning: You've used up over 90% of your quota!");
         } else if percentage_of_max >= 0.75 {
             self.messenger
                 .send("Warning: You've used up over 75% of your quota!");
@@ -72,8 +75,6 @@ use crate::List::{Cons, Nil};
 fn main() {
     let b = Box::new(5);
     println!("b = {}", b);
-
-    // let _list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 
     let x = 5;
     let y = &x;
@@ -95,25 +96,38 @@ fn main() {
                //
     let c = CustomSmartPointer { data: String::from("my stuff") };
 
-    let d = CustomSmartPointer { data: String::from("other stuff") };
+    let _d = CustomSmartPointer { data: String::from("other stuff") };
 
     println!("CustomSmartPointers created");
 
     drop(c);
     println!("CustomSmartPointers dropped before the end of main.");
 
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    println!("count after creating a = {}", Rc::strong_count(&a));
-    let b = Cons(3, Rc::clone(&a));
-    println!("count after creating b = {}", Rc::strong_count(&a));
+    // let _list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 
-    {
-        let c = Cons(4, Rc::clone(&a));
-        println!("count after creating c = {}", Rc::strong_count(&a));
-    }
-    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+    // let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    // println!("count after creating a = {}", Rc::strong_count(&a));
+    // let _b = Cons(3, Rc::clone(&a));
+    // println!("count after creating b = {}", Rc::strong_count(&a));
 
-    RefCell::new(1);
+    // {
+    //     let _c = Cons(4, Rc::clone(&a));
+    //     println!("count after creating c = {}", Rc::strong_count(&a));
+    // }
+    // println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {a:?}");
+    println!("b after = {b:?}");
+    println!("c after = {c:?}");
 }
 
 fn hello(name: &str) {
@@ -123,21 +137,34 @@ fn hello(name: &str) {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::cell::RefCell;
 
     struct MockMessenger {
-        sent_messages: Vec<String>,
+        sent_messages: RefCell<Vec<String>>,
     }
 
     impl MockMessenger {
-        fn new() -> Self {
-            Self { sent_messages: vec![] }
+        fn new() -> MockMessenger {
+            MockMessenger { sent_messages: RefCell::new(vec![]) }
         }
     }
+
     impl Messenger for MockMessenger {
         fn send(&self, message: &str) {
-            self.sent_messages.push(String::from(message));
+            self.sent_messages.borrow_mut().push(String::from(message));
         }
     }
+
+    // ! This FAILS at runtime
+    // impl Messenger for MockMessenger {
+    //     fn send(&self, message: &str) {
+    //         let mut one_borrow = self.sent_messages.borrow_mut();
+    //         let mut two_borrow = self.sent_messages.borrow_mut();
+
+    //         one_borrow.push(String::from(message));
+    //         two_borrow.push(String::from(message));
+    //     }
+    // }
 
     #[test]
     fn it_sends_an_over_75_percent_warning_message() {
@@ -146,6 +173,6 @@ mod tests {
 
         limit_tracker.set_value(80);
 
-        assert_eq!(mock_messenger.sent_messages.len(), 1);
+        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
     }
 }
