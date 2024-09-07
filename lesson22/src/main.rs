6b3946f94a2d8a267b4ecc8ad40ed349616ed280
Prince Muel <vansomecsam@gmail.com>
2024-09-07 02:41:35 +0100

    feat: learn smart pointer Rc (reference counter)

--- lesson22/src/main.rs
+++ lesson22/src/main.rs
@@ -1,7 +1,8 @@
-use std::ops::Deref;
+use std::{ops::Deref, rc::Rc};
 
 pub enum List {
-    Cons(i32, Box<List>),
+    // Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil,
 }
 
@@ -30,13 +31,13 @@ impl Drop for CustomSmartPointer {
     }
 }
 
-use List::{Cons, Nil};
+use crate::List::{Cons, Nil};
 
 fn main() {
     let b = Box::new(5);
     println!("b = {}", b);
 
-    let _list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    // let _list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 
     let x = 5;
     let y = &x;
@@ -64,6 +65,17 @@ fn main() {
 
     drop(c);
     println!("CustomSmartPointers dropped before the end of main.");
+
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
 
 fn hello(name: &str) {
