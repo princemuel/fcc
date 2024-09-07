c9fb7ea8571236c7310c47887ba118ff76548385
Prince Muel <vansomecsam@gmail.com>
2024-09-07 02:05:39 +0100

    feat: learn the Box smart pointer and Deref trait

--- lesson22/src/main.rs
+++ lesson22/src/main.rs
@@ -1,1 +1,53 @@
-fn main() {}
+pub enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(value: T) -> Self {
+        Self(value)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+use std::ops::Deref;
+
+use List::{Cons, Nil};
+
+fn main() {
+    let b = Box::new(5);
+    println!("b = {}", b);
+
+    let _list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    let x = 5;
+    let y = &x;
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let x = 5;
+    let y = MyBox::new(x);
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let x = 5;
+    let y = MyBox::new(x);
+    assert_eq!(5, x);
+    assert_eq!(5, *(y.deref()));
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m); // hello(&(*m)[..]);
+}
+
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
+}
