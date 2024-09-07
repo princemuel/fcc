d18186fbd0a1a1837cbaf2d02c4dfdd38bc9f14f
Prince Muel <vansomecsam@gmail.com>
2024-09-07 21:19:25 +0100

    feat: scaffold first impl of Messenger struct

--- lesson22/src/main.rs
+++ lesson22/src/main.rs
@@ -1,3 +1,4 @@
+use core::str;
 use std::{ops::Deref, rc::Rc};
 
 pub enum List {
@@ -28,6 +29,41 @@ struct CustomSmartPointer {
 impl Drop for CustomSmartPointer {
     fn drop(&mut self) {
         println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
+pub trait Messenger {
+    fn send(&self, message: &str);
+}
+
+pub struct LimitTracker<'a, T: Messenger> {
+    messenger: &'a T,
+    value: usize,
+    max: usize,
+}
+
+impl<'a, T> LimitTracker<'a, T>
+where
+    T: Messenger,
+{
+    pub fn new(messenger: &'a T, max_value: usize) -> Self {
+        Self { messenger, value: 0, max: max_value }
+    }
+
+    pub fn set_value(&mut self, value: usize) {
+        self.value = value;
+
+        let percentage_of_max = (self.value / self.max) as f64;
+
+        if percentage_of_max >= 1.0 {
+            self.messenger.send("Error: You are over your quota!");
+        } else if percentage_of_max >= 0.9 {
+            self.messenger
+                .send("Urgent Warning: You've used up over 90% of your quota!");
+        } else if percentage_of_max >= 0.75 {
+            self.messenger
+                .send("Warning: You've used up over 75% of your quota!");
+        }
     }
 }
 
@@ -76,8 +112,40 @@ fn main() {
         println!("count after creating c = {}", Rc::strong_count(&a));
     }
     println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+
+    RefCell::new(1);
 }
 
 fn hello(name: &str) {
     println!("Hello, {}!", name);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct MockMessenger {
+        sent_messages: Vec<String>,
+    }
+
+    impl MockMessenger {
+        fn new() -> Self {
+            Self { sent_messages: vec![] }
+        }
+    }
+    impl Messenger for MockMessenger {
+        fn send(&self, message: &str) {
+            self.sent_messages.push(String::from(message));
+        }
+    }
+
+    #[test]
+    fn it_sends_an_over_75_percent_warning_message() {
+        let mock_messenger = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+
+        limit_tracker.set_value(80);
+
+        assert_eq!(mock_messenger.sent_messages.len(), 1);
+    }
 }
