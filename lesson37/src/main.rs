09eaed1abb4af1ecd6ffa57c539a6589cc5bd73d
Prince Muel <vansomecsam@gmail.com>
2024-09-10 19:44:27 +0100

    feat(ch19): learn advanced traits in rust

--- lesson37/src/main.rs
+++ lesson37/src/main.rs
@@ -1,1 +1,159 @@
-fn main() {}
+use std::fmt::{self, write, Display};
+use std::ops::Add;
+
+fn main() {
+    let person = Human;
+    person.fly();
+    Pilot::fly(&person);
+    Wizard::fly(&person);
+
+    HomoSapiens::fly();
+
+    <HomoSapiens as Witch>::fly();
+
+    let w = Wrapper(vec![String::from("hello"), String::from("world")]);
+
+    println!("w = {w}");
+}
+
+// pub trait Iterator {
+//     type Item;
+
+//     fn next(&mut self) -> Option<Self::Item>;
+// }
+pub trait Iterator<T> {
+    fn next(&mut self) -> Option<T>;
+}
+
+struct Counter {}
+
+impl Iterator<u32> for Counter {
+    fn next(&mut self) -> Option<u32> {
+        Some(0)
+    }
+}
+
+// impl Iterator for Counter {
+//     type Item = u32;
+
+//     fn next(&mut self) -> Option<Self::Item> {
+//         Some(0)
+//     }
+// }
+
+#[derive(Debug, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Point { x: self.x + rhs.x, y: self.y + rhs.y }
+    }
+}
+
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({},{})", self.x, self.y)
+    }
+}
+
+struct Millimeters(u32);
+struct Meters(u32);
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+
+    fn add(self, rhs: Meters) -> Self::Output {
+        Millimeters(self.0 + (rhs.0 * 1000))
+    }
+}
+
+trait Pilot {
+    fn fly(&self);
+}
+trait Wizard {
+    fn fly(&self);
+}
+
+struct Human;
+
+impl Human {
+    fn fly(&self) {
+        println!("*waving arms furiously*");
+    }
+}
+impl Pilot for Human {
+    fn fly(&self) {
+        println!("This is your captain speaking");
+    }
+}
+
+impl Wizard for Human {
+    fn fly(&self) {
+        println!("up, up and away...");
+    }
+}
+
+struct HomoSapiens;
+
+trait Aeronaut {
+    fn fly();
+}
+trait Witch {
+    fn fly();
+}
+
+impl HomoSapiens {
+    fn fly() {
+        println!("*waving arms furiously*");
+    }
+}
+impl Aeronaut for HomoSapiens {
+    fn fly() {
+        println!("This is your captain speaking");
+    }
+}
+
+impl Witch for HomoSapiens {
+    fn fly() {
+        println!("up, up and away...");
+    }
+}
+
+trait OutlinePrint: fmt::Display {
+    fn outline_print(&self) {
+        let output = self.to_string();
+        let len = output.len();
+        println!("{}", "*".repeat(len + 4));
+        println!("*{}*", " ".repeat(len + 2));
+        println!("* {} *", output);
+        println!("*{}*", " ".repeat(len + 2));
+        println!("{}", "*".repeat(len + 4));
+    }
+}
+
+impl OutlinePrint for Point {}
+
+struct Wrapper(Vec<String>);
+
+impl fmt::Display for Wrapper {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "[{}]", self.0.join(", "))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_adds_points() {
+        assert_eq!(
+            Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
+            Point { x: 3, y: 3 }
+        );
+    }
+}
