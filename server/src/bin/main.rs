6b3f975846949c13106541863a737fe6537a219c
Prince Muel <68931805+princemuel@users.noreply.github.com>
2024-09-12 18:05:53 +0100

    ci: update editor settings files

--- server/src/bin/main.rs
+++ server/src/bin/main.rs
@@ -1,9 +1,8 @@
-use std::io::prelude::*;
+use server::ThreadPool;
+use std::io::{prelude::*, BufReader};
 use std::net::{TcpListener, TcpStream};
 use std::time::Duration;
 use std::{fs, thread};
-
-use server::ThreadPool;
 
 // http://127.0.0.1:7878
 fn main() {
@@ -12,7 +11,7 @@ fn main() {
 
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
 
         pool.execute(|| {
@@ -27,20 +26,32 @@ fn main() {
 //
 // * example: HTTP/1.1 200 OK\r\n\r\n
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
+    // let mut buffer = [0; 1024];
 
-    stream.read(&mut buffer).unwrap();
+    // stream.read(&mut buffer).unwrap();
 
-    let get = b"GET / HTTP/1.1\r\n";
-    let sleep = b"GET /sleep HTTP/1.1\r\n";
+    // let get = b"GET / HTTP/1.1\r\n";
+    // let sleep = b"GET /sleep HTTP/1.1\r\n";
 
-    let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK", "index.html")
-    } else if buffer.starts_with(sleep) {
-        thread::sleep(Duration::from_secs(5));
-        ("HTTP/1.1 200 OK", "sleep.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    // let (status_line, filename) = if buffer.starts_with(get) {
+    //     ("HTTP/1.1 200 OK", "index.html")
+    // } else if buffer.starts_with(sleep) {
+    //     thread::sleep(Duration::from_secs(5));
+    //     ("HTTP/1.1 200 OK", "sleep.html")
+    // } else {
+    //     ("HTTP/1.1 404 NOT FOUND", "404.html")
+    // };
+
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, filename) = match &request_line[..] {
+        | "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        | "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        },
+        | _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
     let contents = fs::read_to_string(filename).unwrap();
--- server/src/lib.rs
+++ server/src/lib.rs
@@ -3,10 +3,15 @@ use std::thread;
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: mpsc::Sender<Message>,
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
+
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
 
 impl ThreadPool {
     /// Create a new ThreadPool.
@@ -37,23 +42,52 @@ impl ThreadPool {
         F: FnOnce() + Send + 'static,
     {
         let job = Box::new(f);
-        self.sender.send(job).unwrap();
+        self.sender.send(Message::NewJob(job)).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        println!("sening terminate message to all workers");
+
+        for _ in &self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        println!("shutting down all workers");
+
+        for worker in &mut self.workers {
+            println!("shutting down {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
     }
 }
 
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
-    fn new(id: usize, reciever: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
+    fn new(id: usize, reciever: Arc<Mutex<mpsc::Receiver<Message>>>) -> Self {
         let thread = thread::spawn(move || loop {
-            let job = reciever.lock().unwrap().recv().unwrap();
-            println!("Worker {id} got a job; executing.");
-            job();
+            let message = reciever.lock().unwrap().recv().unwrap();
+
+            match message {
+                | Message::NewJob(job) => {
+                    println!("Worker {id} got a job; executing.");
+                    job();
+                },
+                | Message::Terminate => {
+                    println!("Worker {id} was told to terminate.");
+                    break;
+                },
+            }
         });
 
-        Self { id, thread }
+        Self { id, thread: Some(thread) }
     }
 }
