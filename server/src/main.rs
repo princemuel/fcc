fb5587196acb380b557fa0f563206c5183d28bc5
Prince Muel <68931805+princemuel@users.noreply.github.com>
2024-09-12 15:59:32 +0100

    feat: return valid html data

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,1 +1,44 @@
-fn main() {}
+use std::fs;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+
+// http://127.0.0.1:7878
+fn main() {
+    let addr = "127.0.0.1:7878";
+    let listener = TcpListener::bind(addr).unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+// * HTTP-Version Status-Code Reason-Phrase CRLF
+// * Headers CRLF
+// * Message Body
+//
+// * example: HTTP/1.1 200 OK\r\n\r\n
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
