591f5a558b049de406cb125fcdf3bb1bb6f86f5f
Prince Muel <68931805+princemuel@users.noreply.github.com>
2024-09-12 17:01:31 +0100

    perf: impl thread pool avoid request blocking

--- server/src/bin/main.rs
+++ server/src/bin/main.rs
@@ -1,16 +1,23 @@
-use std::fs;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::time::Duration;
+use std::{fs, thread};
+
+use server::ThreadPool;
 
 // http://127.0.0.1:7878
 fn main() {
     let addr = "127.0.0.1:7878";
     let listener = TcpListener::bind(addr).unwrap();
 
+    let pool = ThreadPool::new(4);
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -25,9 +32,13 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
         ("HTTP/1.1 200 OK", "index.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "sleep.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
